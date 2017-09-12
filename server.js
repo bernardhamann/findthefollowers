ba908fe0c1e9d1d53a2723aa94c84da634960f47
Bernard <bernard@shipityo.com>
2017-09-12 21:57:05 +0200

    dd

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var clientId = "6be47e7d8ad240f0a3e2fe038e6eaee3";
 var clientSecret = process.env.INSTAGRAM_CLIENTSECRET;
 
 // Your redirect url where you will handle the code param
-var redirectUri = 'http://54.213.76.216:3010/instagram/auth/callback';
+var redirectUri = 'http://54.213.76.216:3010/instagram/auth/callback?';
 // Instagram Auth url
 var instagramAuthUrl = 'https://api.instagram.com/oauth/authorize/?client_id='+ clientId + "&redirect_uri=" + redirectUri + "&response_type=token";
 // https://api.instagram.com/oauth/authorize/?client_id=6be47e7d8ad240f0a3e2fe038e6eaee3&redirect_uri=http://54.213.76.216:3010/instagram/auth/callback&response_type=token
