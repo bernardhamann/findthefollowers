3cc262b6afb46eae9cb6a47351a8a8779a510fb8
Bernard <bernard@shipityo.com>
2017-09-13 09:42:20 +0200

    Change to Explicit Flow Step 1

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var FirebaseConfig = {
     messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
 }
 firebase.initializeApp(FirebaseConfig);
+var InstagramCode = firebase.database().ref('instagram/code');
 var InstagramToken = firebase.database().ref('instagram/token');
 
 // Instagram Auth Codes
@@ -25,10 +26,12 @@ var clientId = "6be47e7d8ad240f0a3e2fe038e6eaee3";
 var clientSecret = process.env.INSTAGRAM_CLIENTSECRET;
 
 // Your redirect url where you will handle the code param
-var redirectUri = 'http://54.213.76.216:3010/instagram/auth/callback?';
+var redirectUri = 'http://54.213.76.216:3010/instagram/auth/callback';
+
+// https://www.instagram.com/developer/authentication/
 // Instagram Auth url
-var instagramAuthUrl = 'https://api.instagram.com/oauth/authorize/?client_id='+ clientId + "&redirect_uri=" + redirectUri + "&response_type=token";
-// https://api.instagram.com/oauth/authorize/?client_id=6be47e7d8ad240f0a3e2fe038e6eaee3&redirect_uri=http://54.213.76.216:3010/instagram/auth/callback&response_type=token
+var instagramAuthUrl = 'https://api.instagram.com/oauth/authorize/?client_id='+ clientId + "&redirect_uri=" + redirectUri + "&response_type=code"
+// "https://api.instagram.com/oauth/authorize/?client_id=6be47e7d8ad240f0a3e2fe038e6eaee3&redirect_uri=http://54.213.76.216:3010/instagram/auth/callback&response_type=code"
 
 // First redirect user to instagram oauth
 app.get('/instagram/auth', function (req, res) {
@@ -39,13 +42,18 @@ app.get('/instagram/auth', function (req, res) {
 // Handle auth code and get access_token for user
 app.get('/instagram/auth/callback', function (req, res) {
   console.log('/instagram/auth/callback');
-  console.log( "1" );
-  var newToken = req.query.access_token ;
-  InstagramToken.set(newToken);
-  res.send(newToken);
+  if (req.code){
+    var newCode;
+    InstagramCode.set(newCode);
+    res.send(newCode);
+  }
+  else {
+    if (error_reason){
+        res.send("Code Error: " + error_reason);
+    }
+    res.send("Code Error");
+  }
 });
-
-// http://54.213.76.216:3010/instagram/auth/callback#access_token=5960480297.6be47e7.716bc8f04c9e462da4c9a08b47940b8c
 
 var server = app.listen(3010, function(){
   var host = server.address().address
